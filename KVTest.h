0342476a9b0746fd3ebcb141e612fe7a44531013
Kaitlyn Vu <kaitlynkvu@kkv.local>
2014-07-12 17:39:31 +1000

    added a test class

---
+++ KVTest.h
@@ -0,0 +1,13 @@
+//
+//  KVTest.h
+//  Git Test
+//
+//  Created by Kaitlyn Vu on 12/07/2014.
+//  Copyright (c) 2014 Kaitlyn Vu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KVTest : NSObject
+
+@end
---
+++ KVTest.m
@@ -0,0 +1,13 @@
+//
+//  KVTest.m
+//  Git Test
+//
+//  Created by Kaitlyn Vu on 12/07/2014.
+//  Copyright (c) 2014 Kaitlyn Vu. All rights reserved.
+//
+
+#import "KVTest.h"
+
+@implementation KVTest
+
+@end
